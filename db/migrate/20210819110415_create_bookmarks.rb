60bcdc1562524ee14843be25aec41a91c0b99c43
Adam Ringrose <whatsyerpoison1@gmail.com>
2021-08-19 12:04:53 +0100

    Add bookmarks model

---
+++ db/migrate/20210819110415_create_bookmarks.rb
@@ -0,0 +1,11 @@
+class CreateBookmarks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :bookmarks do |t|
+      t.string :comment
+      t.integer :movie_id
+      t.integer :list_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_104740) do
+ActiveRecord::Schema.define(version: 2021_08_19_110415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.integer "movie_id"
+    t.integer "list_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
