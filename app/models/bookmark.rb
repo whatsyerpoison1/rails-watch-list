6e3c4338ff86083b07c46280deeffa996e49d95a
Adam Ringrose <whatsyerpoison1@gmail.com>
2021-08-20 12:29:41 +0100

    tried to add card

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, presence: true, length: {minimum: 6}
-
+  validates :movie_id, presence: true, uniqueness: { scope: :list_id, message: "already in the list"}
 end
--- db/migrate/20210819110415_create_bookmarks.rb
+++ db/migrate/20210819110415_create_bookmarks.rb
@@ -2,8 +2,6 @@ class CreateBookmarks < ActiveRecord::Migration[6.0]
   def change
     create_table :bookmarks do |t|
       t.string :comment
-      t.integer :movie_id
-      t.integer :list_id
 
       t.timestamps
     end
